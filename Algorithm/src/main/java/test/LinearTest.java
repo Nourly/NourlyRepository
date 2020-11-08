fbcc62900c768dae9687d76285aceffe44e36976
Nour <690353945@qq.com>
2020-11-08 22:04:12 +0800

    2020/11/8 双链表构建

--- Algorithm/src/main/java/test/LinearTest.java
+++ Algorithm/src/main/java/test/LinearTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import linear.LinkList;
 import linear.SequenceList;
+import linear.TwoWayLinkList;
 
 /**
  * @author Nour
@@ -9,18 +11,24 @@ public class LinearTest {
 
     public static void main(String[] args) {
         //创建顺序表
-        SequenceList<String> s1 = new SequenceList<>(2);
-
+        // SequenceList<String> s1 = new SequenceList<>(2);
+        // LinkList<String> s1 = new LinkList<>();
+        TwoWayLinkList<String> s1 = new TwoWayLinkList<>();
         //测试插入
         s1.insert("1");
         s1.insert("2");
         s1.insert("3");
         s1.insert("4");
         s1.insert("5");
-
+        System.out.println(s1.get(1));
         for (String s : s1) {
             System.out.println(s);
         }
+        //双向链表的分界线
+        System.out.printf("第一个元素是" + s1.getFirst());
+        System.out.printf("最后一个元素是" + s1.getLast());
+        //双向链表的分界线
+
 
         //测试获取
         String getResult = s1.get(1);
